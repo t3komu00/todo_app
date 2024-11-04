64449630881dc0c77bc35f76fd027202111391b3
t3komu00 <your.email@example.com>
2024-11-04 13:23:45 +0200

    todo assignment completed and working upto task 05

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import todoRouter from './routers/todoRouter.js'
+
+const port = process.env.PORT 
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use('/',todoRouter)
+
+/app.use ((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    res.status(statusCode).json({ error: err.message })
+})
+
+
+app.listen(port)
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import { useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import Row from './components/Row';
+
+const url = 'http://localhost:3001'
 
 function App() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    axios.get(url)
+    .then(response => {
+      setTasks(response.data)
+    }).catch(error => {
+      alert(error.response.data.error ? error.response.data.error : error)
+    })
+  }, [])
+
+  const addTask = (task) => {
+    axios.post(url + '/create', {
+      description: task
+    })
+    .then(response => {
+      setTasks([...tasks, {id: response.data.id, description: task}])
+      setTask('')
+    }).catch(error => {
+      alert(error.response.data.error ? error.response.data.error : error)
+    })
+  }
+
+  const deleteTask = (id) => {
+    axios.delete(url + '/delete/' + id)
+    .then(response => {
+      const withoutRemoved = tasks.filter((item)=> item.id !== id)
+      setTasks(withoutRemoved)
+    }).catch(error => {
+      alert(error.response.data.error ? error.response.data.error : error)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id= 'container'> 
+      <h3>Todos</h3>
+      <form>
+        <input
+        placeholder='Add new task'
+        value={task}
+        onChange={e => setTask(e.target.value)}
+        onKeyDown={e => {
+          if(e.key === 'Enter'){
+            e.preventDefault();
+            addTask(task);
+          }
+        }}
+        />
+      </form>
+      <ul>
+        {
+        tasks.map(item => (
+          <Row key={item.id} item={item} deleteTask={deleteTask}/>
+        ))
+        }
+      </ul>
     </div>
   );
 }
